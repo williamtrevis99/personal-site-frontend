2d8cc714467fdaa5e554ac789b28bba0cb4277df
Will <williamherbert.trevis@boeing.com>
2021-03-20 23:13:27 +0000

    frontend work -WT

--- server.js
+++ server.js
@@ -3,17 +3,6 @@ const path = require('path');
 const app = express()
 const port = 8080
 
-// this is an example of middleware - something that intercepts the request
-// and can perform checks / computation on the request
-const requireJSON = () => {
-    return (req, res, next) => {
-        if (req.headers['content-type'] !== 'application/json') {
-            res.status(400).send('Server requires application/json')
-        } else {
-            next()
-        }
-    }
-}
 
 app.use('/', express.static('./dist'))
 
