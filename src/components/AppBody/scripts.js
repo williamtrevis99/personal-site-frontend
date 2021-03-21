3712a2bf4a397bc9acdd9024b167988925720571
Will <williamherbert.trevis@boeing.com>
2021-03-21 13:57:29 +0000

    changing frontend -WT

--- src/components/AppBody/scripts.js
+++ src/components/AppBody/scripts.js
@@ -4,17 +4,27 @@ export default {
 
     async beforeMount() {
         setInterval(async () => {
-            await this.makeGetRequest();
-        }, 4000)
+            this.users = await this.makeGetRequest();
+        }, 1000)
     },
     methods: {
+        clearPost() {
+            document.getElementById('input_text').value = '';
+        },
+        getFullDate() {
+            let today = new Date();
+            let dd = String(today.getDate()).padStart(2, '0');
+            let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+            let yyyy = today.getFullYear();
+
+            return  mm + '/' + dd + '/' + yyyy;
+        },
          makeGetRequest() {
             return new Promise( (resolve, reject) => {
                 try {
-                    console.log("trying get request");
-                    axios.get('http://backend:3333/api/posts/get')
+                    axios.get('http://localhost:3333/api/posts/get')
                         .then(function (res) {
-                            resolve(res)
+                            resolve(res.data.reverse());
                         });
                 } catch (err) {
                     console.log(err);
@@ -23,13 +33,18 @@ export default {
             })
         },
         makePostRequest() {
+            let input = document.getElementById('input_text').value;
+
             return new Promise( (resolve, reject) => {
                 try {
                     console.log("trying get request");
-                    axios.post('http://backend:3333/api/posts/post')
-                        .then(function (res) {
-                            resolve(res)
-                        });
+                    axios.post('http://localhost:3333/api/posts/post', {
+                        name: "Will",
+                        text: input,
+                        time: this.getFullDate(),
+                    }).then(function (res) {
+                        resolve(res)
+                    });
                 } catch (err) {
                     console.log(err);
                     reject(err);
@@ -40,23 +55,7 @@ export default {
 
     data(){
         return {
-            users: [
-                {
-                    "text": "Everybody should read Chaucer to improve their everyday vocabulary.",
-                    "name": "William",
-                    "time": "11:34:90"
-                },
-                {
-                    "text": "yea good point",
-                    "name": "Annie",
-                    "time": "11:34:90"
-                },
-                {
-                    "text": "this is an example",
-                    "name": "Tom",
-                    "time": "11:34:90"
-                },
-            ]
+            users: [],
         }
     }
 }
